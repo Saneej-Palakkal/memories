51f95215a7bffff7905b5212d3d8c4b6371fd916
Saneej Palakkal <palakkalsaneej@gmail.com>
2022-06-04 21:12:12 +0530

    Updates

---
+++ precache-manifest.d8f7fe7813a2021790479997126fc728.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f0dae4d4a112607faab077b2be8a1e5",
+    "url": "/memories/index.html"
+  },
+  {
+    "revision": "6763f2a4bb65566fb6af",
+    "url": "/memories/static/css/main.6fa7be5e.chunk.css"
+  },
+  {
+    "revision": "35ec7b05226f51b97196",
+    "url": "/memories/static/js/2.7fb4f169.chunk.js"
+  },
+  {
+    "revision": "cd94619111e2ab78ce54eac27b302772",
+    "url": "/memories/static/js/2.7fb4f169.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6763f2a4bb65566fb6af",
+    "url": "/memories/static/js/main.a9578e7f.chunk.js"
+  },
+  {
+    "revision": "3a28f6f8ecad883f1cf2",
+    "url": "/memories/static/js/runtime-main.89a99928.js"
+  },
+  {
+    "revision": "dc8bbfe30b9b1cf8867a83e69d051651",
+    "url": "/memories/static/media/memoriesLogo.dc8bbfe3.png"
+  },
+  {
+    "revision": "2b89c5fae2d163d5a126a519646883fa",
+    "url": "/memories/static/media/memoriesText.2b89c5fa.png"
+  }
+]);
